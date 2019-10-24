fae03650b495ba35db9368e86fcff94be8fe338c
adgray09 <alexandergray37@yahoo.com>
2019-10-24 14:58:30 -0700

    add new routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,20 @@ app = Flask(__name__)
 def index():
     """Renders the home page with links to Fortune and Weather."""
     return render_template('index.html')
+
+
+@app.route('/fortune_results')
+def fortune_results():
+    """Displays the user's fortune."""
+    users_gender = request.args.get('gender')
+    # ... etc
+
+    if users_gender == 'male':
+        print("you are a man")
+    return render_template('fortune_results.html')
+        
+        
+        
+        
+        
+        
